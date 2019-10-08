e70bff16360c926912de451d7edfe661ac4b3ffc
codjoe4@gmail.com <codjoe4@gmail.com>
2019-10-07 20:02:03 -0400

    index

---
+++ index.php
@@ -0,0 +1,122 @@
+
+
+<?php
+    /**
+     * Reference: https://getbootstrap.com
+     *            https://startbootstrap.com
+     */
+?>
+
+<!-- Move all this header stuff later -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v = "urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+    <head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+        <title>ggcsocialconnect!</title>
+        <style>
+
+			.masthead {
+            	height: 100vh;
+                min-height: 500px;
+                background-image: url("photos/SocialConnect.png");
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+            }
+        </style>
+    </head>
+
+	<body>
+    	<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+        	<div class="container">
+            	<a class="navbar-brand" href="#page-top">GGC Social Connect</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          			<span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                	<ul class="navbar-nav ml-auto">
+                    	<li class="nav-item active">
+                        	<a href="index.php" class="btn btn-primary" href="#">Home<span class="sr-only">(current)</span></a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Landing -->
+        <header class="masthead">
+            <div class="container h-100">
+            	<div class="row h-100 align-items-center">
+                	<div class="col-12 text-center">
+                    	<h1 class="font-weight-light">CONNECT WITH STUDENTS ON CAMPUS!</h1>
+                        <p class="lead">Looking to Connect? Look no further!</p>
+                        <p>
+            				<a href="CreateEmail.php" class="btn btn-primary my-2">Create Event</a>
+            				<a href="ViewEvent.php" class="btn btn-secondary my-2">View Events</a>
+          				</p>
+                    </div>
+
+            	</div>
+
+        	</div>
+        </header>
+
+        <main role="main">
+          <!-- Section 1 -->
+            <section class="py-5">
+            	<div class="container">
+                	<div class="row">
+                    	<div class="col-md-8 mx-auto">
+                        	<h2 class="font-weight-light">Whatever your style, connect with people your way</h2>
+                          	<p>GGC Social Connect is an online social networking service company based in Lawrenceville, Georgia.
+It was founded by Codjoe Gamadeku, Grace Ongchangco, Elizabeth Haynie and Trivon Etheridge. The founders are students of Georgia
+ Gwinnett College that had a vision
+for a site that allows fellow students to all connect for events and social outings.
+The site displays the campus events, allows its users to create events and has resources to other information about the campus
+ in general.  </p>
+                        </div>
+                	</div>
+                </div>
+            </section>
+            <!-- Section 2 -->
+
+        </main>
+        <!-- Footer -->
+        <footer>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light shadow ">
+                <div class="container">
+                    <a class="navbar-brand" href="#page-top">GGC Social Connect</a>
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+
+                              <li class="nav-item">
+                          <a href="about.php" class="nav-link" href="#">About</a>
+                      </li>
+                              <li class="nav-item">
+                          <a href="Resources.php" class="nav-link" href="#">Resources</a>
+                      </li>
+                              <li class="nav-item">
+                          <a href="Faq.php" class="nav-link" href="#">FAQ</a>
+                      </li>
+
+                          </ul>
+                      </div>
+                  </div>
+              </nav>
+        </footer>
+    </body>
+</html>
